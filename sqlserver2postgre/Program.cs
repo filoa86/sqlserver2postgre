5ae66053a124854ac46829fb8f0d78ebd881631b
filoa86 <filoa86@gmail.com>
2018-08-14 14:04:44 +0200

    start

--- sqlserver2postgre/Program.cs
+++ sqlserver2postgre/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace sqlserver2postgre
 {
@@ -10,6 +12,21 @@ namespace sqlserver2postgre
     {
         static void Main(string[] args)
         {
+            var SQL_SERVER = ConfigurationManager.AppSettings["SQL_SERVER_ADDRESS"];
+            var SQL_DATABASE = ConfigurationManager.AppSettings["SQL_SERVER_ADDRESS"];
+            var SQL_USERID = ConfigurationManager.AppSettings["SQL_SERVER_ADDRESS"];
+            var SQL_PASSWORD = ConfigurationManager.AppSettings["SQL_SERVER_ADDRESS"];
+            // connection string sql server
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+            "Data Source=" + SQL_SERVER +
+            ";Initial Catalog=" + SQL_DATABASE  +
+            ";User id=UserName;" + SQL_USERID +
+            ";Password=Secret" + SQL_PASSWORD + ";";
+            //conn.Open();
+
+            // connection string postgresql
         }
     }
 }
+
